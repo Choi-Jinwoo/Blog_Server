2c77dc0935cf9d480f48e5812331895daf30b3fb
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-19 16:18:42 +0900

    [ADD] Comment - Modify

---
+++ src/api/v1/comment/comment.ctrl/modifyComment.ts
@@ -0,0 +1,91 @@
+import { Response } from 'express';
+import AuthRequest from '../../../../type/AuthRequest';
+import { getRepository } from 'typeorm';
+import { validateModify } from '../../../../lib/validation/comment';
+import logger from '../../../../lib/logger';
+import User from '../../../../entity/User';
+import Comment from '../../../../entity/Comment';
+import Post from '../../../../entity/Post';
+
+export default async (req: AuthRequest, res: Response) => {
+  if (!validateModify(req, res)) return;
+
+  const user: User = req.user;
+  const idx: number = Number(req.params.idx);
+  if (isNaN(idx)) {
+    logger.yellow('검증 오류.', 'idx is NaN');
+    res.status(400).json({
+      message: '검증 오류.',
+    });
+    return;
+  }
+
+  type RequestBody = {
+    content: string;
+  }
+
+  const { content }: RequestBody = req.body;
+
+  try {
+    const commentReo = getRepository(Comment);
+    const comment: Comment = await commentReo.findOne({
+      where: {
+        idx,
+      },
+    });
+
+    if (!comment) {
+      logger.yellow('댓글 없음.');
+      res.status(404).json({
+        message: '댓글 없음.',
+      });
+      return;
+    }
+
+    const postRepo = getRepository(Post);
+    const post: Post = await postRepo.findOne({
+      where: {
+        idx: comment.fk_post_idx,
+        is_deleted: false,
+      },
+    });
+
+    if (!post) {
+      logger.yellow('글 없음.');
+      res.status(404).json({
+        message: '글 없음.',
+      });
+      return;
+    }
+
+    if (post.is_private) {
+      if (!user.is_admin) {
+        logger.yellow('권한 없음(비공개 글)');
+        res.status(403).json({
+          message: '비공개 글.',
+        });
+        return;
+      }
+    }
+
+    if (comment.fk_user_id !== user.id) {
+      logger.yellow('권한 없음.');
+      res.status(403).json({
+        message: '권한 없음.',
+      });
+      return;
+    }
+
+    comment.content = content;
+    await commentReo.save(comment);
+    res.status(200).json({
+      message: '댓글 수정 성공.',
+    });
+    return;
+  } catch (err) {
+    logger.red('댓글 수정 서버 오류.', err.message);
+    res.status(500).json({
+      message: '서버 오류.',
+    });
+  }
+}
--- src/api/v1/comment/index.ts
+++ src/api/v1/comment/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authMiddleware from '../../../lib/middleware/auth';
 import createComment from './comment.ctrl/createComment';
+import modifyComment from './comment.ctrl/modifyComment';
 
 const router = Router();
 
 router.post('/', authMiddleware.guest, createComment);
+router.put('/:idx', authMiddleware.user, modifyComment);
 
 export default router;
