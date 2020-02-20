f22e3781b3488b3112b942bf5ee5bc0dd55c31c5
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-21 00:48:55 +0900

    [ADD] reply - get

--- src/api/v1/reply/reply.ctrl/getReplies.ts
+++ src/api/v1/reply/reply.ctrl/getReplies.ts
@@ -3,24 +3,40 @@ import AuthRequest from '../../../../type/AuthRequest';
 import { getRepository } from 'typeorm';
 import logger from '../../../../lib/logger';
 import User from '../../../../entity/User';
-import Post from '../../../../entity/Post';
 import Comment from '../../../../entity/Comment';
+import Post from '../../../../entity/Post';
+import Reply from '../../../../entity/Reply';
 
 export default async (req: AuthRequest, res: Response) => {
   const user: User = req.user;
-  const postIdx: number = req.query.post;
+  const commentIdx: number = req.query.comment;
 
   try {
+    const commentRepo = getRepository(Comment);
+    const comment: Comment = await commentRepo.findOne({
+      where: {
+        idx: commentIdx,
+      },
+    });
+
+    if (!comment) {
+      logger.yellow('댓글 없음.');
+      res.status(404).json({
+        message: '댓글 없음.',
+      });
+      return;
+    }
+
     const postRepo = getRepository(Post);
     const post: Post = await postRepo.findOne({
       where: {
-        idx: postIdx,
+        idx: comment.fk_post_idx,
         is_deleted: false,
       },
     });
 
     if (!post) {
-      logger.yellow('글 없음');
+      logger.yellow('글 없음.');
       res.status(404).json({
         message: '글 없음.',
       });
@@ -37,22 +53,22 @@ export default async (req: AuthRequest, res: Response) => {
       }
     }
 
-    const commentRepo = getRepository(Comment);
-    const comments: Comment[] = await commentRepo.find({
+    const replyRepo = getRepository(Reply);
+    const replies: Reply[] = await replyRepo.find({
       where: {
-        post,
+        comment,
       },
     });
 
-    logger.green('댓글 목록 조회 성공.');
+    logger.yellow('답글 목록 조회 성공.');
     res.status(200).json({
-      message: '댓글 목록 조회 성공.',
+      message: '답글 목록 조회 성공.',
       data: {
-        comments,
+        replies,
       },
     });
   } catch (err) {
-    logger.red('댓글 목록 조회 서버 오류', err.message);
+    logger.red('답글 목록 조회 서버 오류.', err.message);
     res.status(500).json({
       message: '서버 오류.',
     });
