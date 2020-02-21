6f728353371654dd419001ee8c62abd6080841df
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-21 15:07:04 +0900

    [ADD] AuthCode entity

--- src/entity/AuthCode.ts
+++ src/entity/AuthCode.ts
@@ -4,15 +4,25 @@ import {
   BaseEntity,
   CreateDateColumn,
   PrimaryColumn,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity('subscribe')
-export default class Subscribe extends BaseEntity {
-  @PrimaryColumn({
+@Entity('auth_code')
+export default class AuthCode extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column({
     length: 255,
     nullable: false,
   })
   email: string;
+
+  @Column({
+    length: 45,
+    nullable: false,
+  })
+  code: string;
 
   @Column('timestampz')
   @CreateDateColumn()
