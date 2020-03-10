e076a45e5b121ba3dd8e106f6adf29d482034182
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-11 02:25:21 +0900

    [ADD] Notice List Get Order By created_at

--- src/api/v1/notice/index.ts
+++ src/api/v1/notice/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import authMiddleware from '../../../lib/middleware/auth';
+import getNotices from './notice.ctrl/getNotices';
 import createNotice from './notice.ctrl/createNotice';
 import modifyNotice from './notice.ctrl/modifyNotice';
+import deleteNotice from './notice.ctrl/deleteNotice';
 
 const router = Router();
 
+router.get('/', getNotices);
 router.post('/', authMiddleware.admin, createNotice);
 router.put('/:idx', authMiddleware.admin, modifyNotice);
+router.delete('/:idx', authMiddleware.admin, deleteNotice);
 
 export default router;
