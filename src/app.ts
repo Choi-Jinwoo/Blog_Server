432f104d0809b4bbc8e6930e67bd34b0a2737674
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-17 14:11:35 +0900

    [ADD] Deny unallowed IP

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import api from './api';
 import path from 'path';
+import denyUnAllowed from './lib/middleware/denyUnAllowed';
 
 const app = express();
+
+app.use(denyUnAllowed);
 
 app.use(cors());
 app.use(bodyParser.json());
