7a05c7574b9886b2f0c64239606bd26112d1a6f7
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-22 14:55:57 +0900

    [ADD] HTTPS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
+import http from 'http';
 import app from './app';
 import * as database from './database';
 import logger from './lib/logger'
@@ -7,6 +11,16 @@ const { PORT } = process.env;
 
 database.getConnection();
 
-app.listen(PORT || 8080, () => {
-  logger.green(`wlswoo-blog server is listening to ${PORT}`);
+const option = {
+  ca: fs.readFileSync('/etc/letsencrypt/live/api.wlswoo.com/fullchain.pem'),
+  key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/api.wlswoo.com/privkey.pem'), 'utf8').toString(),
+  cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/api.wlswoo.com/cert.pem'), 'utf8').toString(),
+};
+
+https.createServer(option, app).listen(443, () => {
+  logger.green(`[HTTPS] wlswoo-blog server is listening to ${PORT}`);
 });
+
+http.createServer(app).listen(PORT || 8080, () => {
+  logger.green(`[HTTP] wlswoo-blog server is listening to ${PORT}`);
+})
