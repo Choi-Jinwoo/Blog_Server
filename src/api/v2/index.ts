0de33f97018acb1fd8b6fb7058bb5f52a6b42eea
Choi-Jinwoo <49791336+Choi-Jinwoo@users.noreply.github.com>
2020-04-10 14:27:52 +0900

    [FIX] Delete Comment/Reply/Profile API

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
+
 import auth from './auth';
 import category from './category';
 import upload from './upload';
 import post from './post'
-import comment from './comment';
-import reply from './reply';
 import file from './file';
 import subscription from './subscription';
-import profile from './profile';
 import notice from './notice';
 
 const router = Router();
@@ -16,11 +14,8 @@ router.use('/auth', auth);
 router.use('/category', category);
 router.use('/upload', upload);
 router.use('/post', post);
-router.use('/comment', comment);
-router.use('/reply', reply);
 router.use('/file', file);
 router.use('/subscription', subscription);
-router.use('/profile', profile);
 router.use('/notice', notice);
 
 export default router;
