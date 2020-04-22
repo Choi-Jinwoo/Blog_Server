684112ccb38ca01985f8b245a0a34a67f792d92f
Choi-Jinwoo <49791336+Choi-Jinwoo@users.noreply.github.com>
2020-04-22 23:26:29 +0900

    [FIX] Config NODE_ENV [localhost / blog.wlswoo.com]

--- src/lib/util/generateURL.ts
+++ src/lib/util/generateURL.ts
@@ -1,3 +1,8 @@
+import 'dotenv/config';
+
 export default (req, fileName: string) => {
+  if (process.env.ENV === 'DEV')
+    return `http://localhost:8080/public/${fileName}`;
+
   return `https://api.wlswoo.com/public/${fileName}`;
 }
